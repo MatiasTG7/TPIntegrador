da0a15b1daa7dabe540df90fa4fe4034aa2dc707
TG <matutemariano@gmail.com>
2025-11-12 19:26:12 -0300

    dale gaspaaar

--- jscript.js
+++ jscript.js
@@ -1,28 +1,4 @@
-let imagenes = [
-  "img/samsung.jpg",
-  "img/apple.jpg",
-  "img/xiaomi.jpg"
-];
-let indice = 0;
-
-function mostrarImagen() {
-  const img = document.getElementById("carrousel");
-  if (img) img.src = imagenes[indice];
-}
-
-function siguiente() {
-  indice = (indice + 1) % imagenes.length;
-  mostrarImagen();
-}
-
-function anterior() {
-  indice = (indice - 1 + imagenes.length) % imagenes.length;
-  mostrarImagen();
-}
-
-setInterval(siguiente, 4000);
-
-document.addEventListener("DOMContentLoaded", () => {
+ocument.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("formContacto");
   if (!form) return;
 
@@ -35,27 +11,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const telregex = /^\d{8,15}$/;
     const mensaje = document.getElementById("mensaje").value.trim();
     
+    let valid = true;
     const errores = [];
     const resultado = document.getElementById("resultado");
     resultado.innerHTML = "";
     document.getElementById('telefonoError').textContent ="";
+    
     if(!telefono){
       document.getElementById('telefonoError').textContent = "el telefono es obligatorio";
       valid = false;
     }else if(!telregex.test(telefono)){
-      document.getElementById('telefonoError') = 'numero no valido. porfavor asegurarse de escribirlo bien';
+      document.getElementById('telefonoError').textContent = 'numero no valido. porfavor asegurarse de escribirlo bien';
       valid = false;
     }
+    
     if (nombre === "" || nombre.length > 50)
       errores.push("⚠ El nombre es obligatorio y debe tener menos de 50 caracteres.");
+    
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
       errores.push("⚠ Correo electrónico inválido.");
-    if (!/^\d{10}$/.test(telefono))
-      errores.push("⚠ El teléfono debe tener 10 dígitos.");
+      
     if (mensaje === "")
       errores.push("⚠ El mensaje no puede estar vacío.");
 
-    if (errores.length > 0) {
+    if (errores.length > 0 || !valid) {
       errores.forEach(err => {
         const p = document.createElement("p");
         p.style.color = "red";
